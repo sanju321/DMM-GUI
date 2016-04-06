f54f7fe39142bf9dfe618f50d86bfb30ae170076
Azoi <jainsanjujain94@gmail.com>
2016-04-06 17:36:05 +0530

    plot and dmm mechanism added

---
+++ dmm_monitor.py
@@ -0,0 +1,115 @@
+import Queue, threading, time, serial
+
+class DmmMonitorThread(threading.Thread):
+    """ A thread for monitoring a DMMUSB port. The DMMUSB port is 
+        opened when the thread is started.
+    
+        data_q:
+            list for received data. Items in the queue are
+            received data from dmm respective of voltage,current
+            ,resistance.
+        
+        error_q:
+            list for error messages. In particular, if the 
+            serial port fails to open for some reason, an error
+            is placed into this queue.
+        
+        measure_input:
+            voltage/current/resistance
+    """
+    
+    def __init__(   self, data_q, error_q,measure_input):
+        threading.Thread.__init__(self)
+        
+        self.serial_port = None
+        
+        self.data_q   = data_q
+        self.error_q  = error_q
+        
+        self.measure_input=measure_input
+        
+        if self.measure_input=="voltage":
+            self.measure_cmd="VDC\n"
+
+        if self.measure_input=="current":
+            self.measure_cmd="ADC\n"
+        
+        if self.measure_input=="resistance":
+            self.measure_cmd="OHMS\n"
+        
+
+        self.alive    = threading.Event()
+        self.alive.set()
+    #------------------------------------------------------
+    def DMM_capture_init(self):
+
+        '''sets DMM for reading values. 
+        global variables--DMM_serial_comm
+        @return:True/False
+        '''
+        self.serial_port.write("\n")
+        time.sleep(0.4)
+        self.serial_port.flushInput()
+
+        self.serial_port.write(self.measure_cmd)
+        ip = self.serial_port.readline()
+        
+        if ip == '=>\r\n':
+            print "GO"
+            time.sleep(0.1)
+            self.serial_port.flushInput()
+            self.serial_port.write("LWLS\n")
+            ip = self.serial_port.readline()
+            if ip == '=>\r\n':
+                return True
+            else:
+                return False
+        else:
+            return False
+
+    def run(self):
+        
+        try:
+            if self.serial_port: 
+                self.serial_port.close()
+            for i in range(0,50):
+                print "value of i",i
+                try:
+                    self.serial_port = serial.Serial(port='/dev/ttyUSB'+str(i), baudrate=9600,timeout = 10)
+                    print 80*'-',"DMM_serial_comm : ",'/dev/ttyUSB'+str(i),'\n',80*'-'
+                    break
+                except:
+                    nothing = 1
+        except Exception as e:
+            self.error_q.append(e.message)
+            return
+        print self.serial_port
+        if not self.DMM_capture_init():
+            return
+
+        # Restart the clock
+        startTime = time.time()
+        
+        while self.alive.isSet():
+            
+            print time.time()
+            time.sleep(0.01)
+            self.serial_port.flushInput()
+            print "going to read value"
+            self.serial_port.write("MEAS1?\n")#for dc voltage meter measurement
+            dmm_output = self.serial_port.readline()
+
+            print "dmm_output  ",dmm_output
+            timestamp = time.time() - startTime
+            print "timestamp   ",timestamp
+            #timestamp = time.clock()
+            self.data_q.append((dmm_output,timestamp))
+            self.serial_port.flushInput()
+        # clean up
+        if self.serial_port:
+            self.serial_port.close()
+
+    def join(self, timeout=None):
+        self.alive.clear()
+        threading.Thread.join(self, timeout)
+
--- example_1.py
+++ example_1.py
@@ -6,21 +6,68 @@ from matplotlib.figure import Figure
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
 import PyQt4.Qwt5 as Qwt
+from dmm_monitor import DmmMonitorThread
+from PyQt4.Qwt5.anynumpy import *
+import random
+from PyQt4 import Qt
+
 
 class PlottingDataMonitor(QMainWindow):
     def __init__(self, parent=None):
         super(PlottingDataMonitor, self).__init__(parent)
 
         self.monitor_active = False 
+        self.dmm_monitor=None
+        self.curve=[None]
+        self.temperature_samples = []
 
         self.timer = QTimer()
+        self.create_menu()
         self.create_main_frame()
         self.create_status_bar()
+        self.create_panner()
         # Activate start-stop button connections
         self.connect(self.button_Connect, SIGNAL("clicked()"),
                     self.OnStart)
         self.connect(self.button_Disconnect, SIGNAL("clicked()"),
                     self.OnStop)
+
+    def save_plot(self):
+		file_choices = "PNG (*.png)|*.png"
+		path = unicode(QFileDialog.getSaveFileName(self,'Save file', '',file_choices))
+		if path:
+			i = QtGui.QImage()
+			i.save(path)
+			# self.canvas.print_figure(path, dpi=self.dpi)
+			self.statusBar().showMessage('Saved to %s' % path, 2000)
+    	
+    def create_action(self,text,slot=None,shortcut=None,icon=None,tip=None,checkable=False,signal="triggered()"):
+    	action = QAction(text, self)
+        if icon is not None:
+            action.setIcon(QIcon(":/%s.png" % icon))
+        if shortcut is not None:
+            action.setShortcut(shortcut)
+        if tip is not None:
+            action.setToolTip(tip)
+            action.setStatusTip(tip)
+        if slot is not None:
+            self.connect(action, SIGNAL(signal), slot)
+        if checkable:
+            action.setCheckable(True)
+        return action
+
+    def add_actions(self, target, actions):
+		for action in actions:
+			if action is None:
+			    target.addSeparator()
+			else:
+			    target.addAction(action)
+
+    def create_menu(self):
+		self.file_menu = self.menuBar().addMenu("&File")
+		#load_file_action = self.create_action("&Save plot",shortcut="Ctrl+S", slot=self.save_plot, tip="Save the plot")
+		load_file_action=self.create_action("&Save plot",shortcut="Ctrl+S",slot=self.save_plot,tip="Save the Plot")
+		self.add_actions(self.file_menu, (load_file_action,None))
 
     def create_com_box(self):
 		""" 
@@ -50,25 +97,53 @@ class PlottingDataMonitor(QMainWindow):
 
     def create_plot(self):
         plot = Qwt.QwtPlot(self)
-        plot.setCanvasBackground(Qt.black)
+        plot.setCanvasBackground(Qt.Qt.black)
         plot.setAxisTitle(Qwt.QwtPlot.xBottom, 'Time')
-        plot.setAxisScale(Qwt.QwtPlot.xBottom, 0, 10, 1)
-        plot.setAxisTitle(Qwt.QwtPlot.yLeft, 'Voltage')
-        plot.setAxisScale(Qwt.QwtPlot.yLeft, 0, 250, 40)
+        #plot.setAxisScale(Qwt.QwtPlot.xBottom, 0, 10, 1)
+        plot.setAxisTitle(Qwt.QwtPlot.yLeft, 'voltage')
+        #plot.setAxisScale(Qwt.QwtPlot.yLeft, 0, 30, 5)
         plot.replot()
-        return plot
+
+        self.zoomer = Qwt.QwtPlotZoomer(plot.canvas())
+        self.zoomer.setMousePattern(Qwt.QwtEventPattern.MouseSelect2,
+                               Qt.Qt.RightButton, Qt.Qt.ControlModifier)
+        self.zoomer.setMousePattern(Qwt.QwtEventPattern.MouseSelect3,
+                               Qt.Qt.RightButton)
+        self.zoomer.setRubberBandPen(Qt.Qt.darkBlue)
+        self.zoomer.setTrackerPen(Qt.Qt.darkBlue)
+        self.panner = Qwt.QwtPlotPanner( plot.canvas() )
+        self.panner.setAxisEnabled(Qwt.QwtPlot.yRight, False)
+        self.panner.setAxisEnabled(Qwt.QwtPlot.xBottom, False)
+
+        self.panner.setMouseButton (Qt.Qt.LeftButton)
+        curve = [None]*1
+        pen = [QPen(QColor('limegreen')) ]
+        
+        curve[0] =  Qwt.QwtPlotCurve('')
+        curve[0].setRenderHint(Qwt.QwtPlotItem.RenderAntialiased)
+        pen[0].setWidth(2)
+        curve[0].setPen(pen[0])
+        curve[0].attach(plot)
+
+        return plot,curve
 
     def create_status_bar(self):
         self.status_text = QLabel('Monitor idle')
         self.statusBar().addWidget(self.status_text, 1)
 
+
+    def create_panner(self):
+        pass
+        # self.panner.setMouseButton(Qt.Qt.MidButton)
+        
     def create_main_frame(self):
 
     	#start and stop reading button
     	portname_layout = self.create_com_box()
+
     	self.com_box.setLayout(portname_layout)
     	# Plot 
-        self.plot = self.create_plot()
+        self.plot,self.curve = self.create_plot()
         plot_layout = QVBoxLayout()
         plot_layout.addWidget(self.plot)
         plot_groupbox = QGroupBox('Plot')
@@ -83,21 +158,41 @@ class PlottingDataMonitor(QMainWindow):
         self.main_frame.setLayout(main_layout)
         
         self.setCentralWidget(self.main_frame)
+
+
     
     def OnStart(self):
     	self.button_Connect.setEnabled(False)
         self.button_Disconnect.setEnabled(True)
+        self.measure_input=''
+        self.temperature_samples=[]
         if not self.measure_voltage.isChecked():
         	self.measure_voltage.setEnabled(False)
+        else:
+        	self.measure_input="voltage"	
+    	
     	if not self.measure_current.isChecked():
         	self.measure_current.setEnabled(False)
+        else:
+        	self.measure_input="current"	
+        
         if not self.measure_resistance.isChecked():
         	self.measure_resistance.setEnabled(False)
+        else:
+        	self.measure_input="resistance"
 
         self.monitor_active = True
+        self.plot.setAxisTitle(Qwt.QwtPlot.yLeft, self.measure_input)
+        self.data_q      =  []
+        self.error_q     =  []
+        self.dmm_monitor =  DmmMonitorThread(self.data_q,self.error_q,self.measure_input)
+        print self.dmm_monitor
+        self.dmm_monitor.start()
 
         self.connect(self.timer, SIGNAL('timeout()'), self.on_timer)
-            	
+        self.timer.start(1000)
+
+        self.status_text.setText('Monitor running')
     
     def OnStop(self):
     	self.button_Connect.setEnabled(True)
@@ -108,14 +203,46 @@ class PlottingDataMonitor(QMainWindow):
         	self.measure_current.setEnabled(True)
         if not self.measure_resistance.isChecked():
         	self.measure_resistance.setEnabled(True)
+        if self.dmm_monitor is not None:
+            self.dmm_monitor.join(1000)
+            self.dmm_monitor = None
+        self.monitor_active = False
+
+        self.timer.stop()
+        self.status_text.setText('Monitor idle')
 
     def on_timer(self):
         """ Executed periodically when the monitor update timer
             is fired.
         """
-        self.read_serial_data()
-        self.update_monitor()
+        print "temp samples   ",len(self.temperature_samples)
+        print "data_q length    ",len(self.data_q)
+        if len(self.data_q)!=0:
+	        # print "data output  ",self.data_q 
+	        # print "recent data  ",self.data_q[-1]
+	        # tdata=[float(self.data_q[-1][1])]
+	        # data_output=[float(self.data_q[-1][0].split(' ')[0])]
+	        # print "tdata  ",tdata
+	        # print "data_output  ",data_output
+	        # self.x = arange(0.0, 100.1, 0.5)
+	        # print "x value  ",self.x
+	        # self.y = zeros(len(self.x), Float)
+	        # print "y value  ",self.y
+	        # self.y = concatenate((self.y[:1], self.y[:-1]), 1)
+        	# self.y[0] = random.random()
+	        # self.curve[0].setData(self.x,self.y)
 
+			self.temperature_samples.append((float(self.data_q[-1][1]),float(self.data_q[-1][0].split(' ')[0])))
+			xdata = [s[0] for s in self.temperature_samples]
+			ydata = [s[1] for s in self.temperature_samples]
+			self.plot.setAxisScale(Qwt.QwtPlot.xBottom,xdata[0],max(20,xdata[-1]))
+			# self.plot.setAxisScale(Qwt.QwtPlot.xBottom, xdata[0], max(20, xdata[-1]))
+			self.curve[0].setData(xdata, ydata)
+			#self.plot.setAxisScale(Qwt.QwtPlot.xBottom, float(self.data_q[-1][1]), float(self.data_q[-1][0].split(' ')[0]) )        
+
+			self.plot.replot()
+
+	
 def main():
     app = QApplication(sys.argv)
     form = PlottingDataMonitor()
